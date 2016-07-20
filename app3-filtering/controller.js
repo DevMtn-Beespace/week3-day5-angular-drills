a8d8b0aa46dccb5c2654fa50181f17d6a19a27b6
mlecke <mlecke@beespace.com>
2016-07-20 17:26:43 -0600

    better filter

--- app3-filtering/controller.js
+++ app3-filtering/controller.js
@@ -1,6 +1,6 @@
 angular.module('filterApp')
 .controller('filterCtrl', ['$scope', 'filterSvc', function($scope, filterSvc){
-$scope.inputLabel = "Filter by input:";
+
 $scope.data = filterSvc.data;
 console.log($scope.data);
 
